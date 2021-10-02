68cd7ccacb349b12cc1c09efe7d7791c18c49385
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2021-10-02 19:10:57 +0900

    implemented form validation

---
+++ script.js
@@ -0,0 +1,57 @@
+const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const message = document.getElementById("message");
+const messageContainer = document.querySelector(".message-container");
+
+let isValid = false;
+let passwordsMatch = false;
+
+function validateForm() {
+  isValid = form.checkValidity();
+  if (!isValid) {
+    message.textContent = "Please fill out all fields";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColo = "green";
+    password2El.style.borderColor = "green";
+  } else {
+    passwordsMatch = false;
+    message.textContent = "Password did not match";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    password1El.style.borderColo = "red";
+    password2El.style.borderColor = "red";
+    return;
+  }
+  if (isValid && passwordsMatch) {
+    message.textContent = "Successfully registered!";
+    message.style.color = "green";
+    messageContainer.style.borderColor = "green";
+  }
+}
+
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  console.log(user);
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  validateForm();
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
+}
+
+form.addEventListener("submit", processFormData);
